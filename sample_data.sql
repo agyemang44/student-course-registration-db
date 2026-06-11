a2a2bb517cb3b84ee3290f80993989ff58aca8db
agyemang44 <keagyemang010@gmail.com>
2026-06-11 08:21:45 +0000

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO students VALUES 
+(1, 'Ama Boateng', 'Computer Science', 3),
+(2, 'Kwame Asante', 'Data Science', 2);
+
+INSERT INTO courses VALUES 
+(101, 'CS301', 'Database Systems', 3),
+(102, 'CS302', 'Data Structures', 3);
